150ca05017de39caded7f16659bc39b868cdc54f
Evan Taylor <admin@evantaylor.ca>
2021-04-28 20:45:26 -0700

    Fixed typos in erc1155 interface

--- lib/ethereum-contract-abi/contract/eip/erc1155_metadata_interface.rb
+++ lib/ethereum-contract-abi/contract/eip/erc1155_metadata_interface.rb
@@ -14,13 +14,13 @@ module EthereumContractABI
           contract.has_function?(self.uri)
         end
 
-        def function
+        def functions
           [self.uri]
         end
 
         def self.uri
           function_name = 'uri'
-          inputs = [Output.new(AbiTypes::Uint.new, '_id')]
+          inputs = [Input.new(AbiTypes::Uint.new, '_id')]
           outputs = [Output.new(AbiTypes::String.new)]
           Function.new(function_name, inputs, outputs)
         end
