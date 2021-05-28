9226dea66379cabfd04d4433d80b7a125e4b406c
Evan Taylor <admin@evantaylor.ca>
2021-05-27 21:37:03 -0700

    Added enumerable constant

--- lib/ethereum-contract-abi/contract/eip/constants.rb
+++ lib/ethereum-contract-abi/contract/eip/constants.rb
@@ -2,6 +2,7 @@ module EthereumContractABI
   module ContractInterface
     module EIP
       ERC721_METADATA_ID = '0x5b5e139f'
+      ERC721_ENUMERABLE_ID = '0x780e9d63'
       ERC1155_METADATA_ID = '0x0e89341c'
       ERC165_ID = 'ERC165'
     end
